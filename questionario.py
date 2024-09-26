9019644c987150d3ce7ddf83c37133bcbe830aff
✧˖°.Xorginho✧˖°. <144127740+xorginhoo@users.noreply.github.com>
2024-09-26 11:14:55 -0300

    Create questionario.py

---
+++ questionario.py
@@ -0,0 +1,40 @@
+#Questão 1
+num1 = 5
+num2 = 7
+
+print (num1)
+print (num2)
+
+#Questão 2
+num1 = 10
+num2 = 20
+
+resultado = num1 - num2
+print(resultado)
+
+#Questão 3
+num1 = 3
+num2 = 4
+
+resultado = num1 * num2
+print (resultado)
+
+#Questão 4
+dividendo = 100
+divisor = 5
+
+resultado = dividendo / divisor
+print (resultado)
+
+#Questão 5
+largura = 5
+altura = 10
+
+resultado = largura * altura
+print (resultado)
+
+#Questão 6
+#Questão 7
+#Questão 8
+#Questão 9
+#Questão 10
