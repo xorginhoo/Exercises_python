3cb2a5f171a9bc4d3783b471f88d933b228d4b76
✧˖°.Xorginho✧˖°. <144127740+xorginhoo@users.noreply.github.com>
2024-10-03 08:39:41 -0300

    Update questionario.py

--- questionario.py
+++ questionario.py
@@ -34,7 +34,33 @@ resultado = largura * altura
 print (resultado)
 
 #Questão 6
+raio = 3
+circuferencia = 2*3.14*raio
+
+print (circuferencia)
+
 #Questão 7
+raio = 4
+vesfera = (4/3)*3.14*raio**3
+
+print (vesfera)
+
 #Questão 8
+celsius = 20
+fahrenheit = ((celsius * 9/5) + 32)
+
+print (fahrenheit)
+
 #Questão 9
+total = 200
+percentual= 25
+
+resultado = ((total*percentual)/100)
+
+print (resultado)
+
 #Questão 10
+lado = 6
+perimetro=4*lado
+
+print (perimetro)
