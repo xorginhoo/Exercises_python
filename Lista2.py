1e19c96bf7aaad8dd816348c788821b3d474623e
✧˖°.Xorginho✧˖°. <144127740+xorginhoo@users.noreply.github.com>
2024-10-05 09:43:51 -0300

    Create Lista2.py

---
+++ Lista2.py
@@ -0,0 +1,10 @@
+#1) Faça um Programa que peça e mostre a mensagem "Alo mundo" na tela.
+print ("Alo mundo")
+
+#2) Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+num = input("Digite um número: ")
+print(f"o número escolhido foi o número {num}")
+#ou
+print ("o número escolhido foi o número "+ str(num))
+
+#3) Faça um Programa que peça dois números e imprima a soma.
