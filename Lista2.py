4f49d86b0be65c92da8e88268b6e3ca60406ac5f
✧˖°.Xorginho✧˖°. <144127740+xorginhoo@users.noreply.github.com>
2024-10-08 08:38:00 -0300

    Update Lista2.py

--- Lista2.py
+++ Lista2.py
@@ -71,4 +71,68 @@ F = C * (9/5) + 32
 
 print (f"a temperatura {C:.1f} Celsius em fahrenheit é {F:.2f}")
 
+"""
+11. Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+a. o produto do dobro do primeiro com metade do segundo .
+b. a soma do triplo do primeiro com o terceiro.
+c. o terceiro elevado ao cubo.
+"""
+n1 = int(input("Digite um número inteiro: "))
+n2 = int(input("Digite outro número inteiro: "))
+n3 = float(input("Digite um número real: "))
 
+print("Produto do dobro do primeiro com metade do segundo:", (2 * n1) * (n2 / 2))
+print("Soma do triplo do primeiro com o terceiro:", (3 * n1) + n3)
+print("Terceiro elevado ao cubo:", n3 ** 3)
+
+"""
+12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule
+seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+"""
+h = float(input("Digite sua altura: "))
+
+print (f"Seu peso ideal é {(72.7*h) - 58:.2f}")
+
+
+"""
+13. Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule
+seu peso ideal, utilizando as seguintes fórmulas:
+a. Para homens: (72.7*h) - 58
+b. Para mulheres: (62.1*h) - 44.7
+"""
+h = float(input("Digite sua altura: "))
+
+print("masculino:")
+print (f"a) Seu peso ideal é {(72.7*h) - 58:.2f}")
+print("")
+print("feminino:")
+print (f"b) Seu peso ideal é {(62.1*h) - 44.7:.2f}")
+
+"""
+14. João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o
+rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o
+estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar
+uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que
+
+leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a
+quantidade de quilos além do limite e na variável multa o valor da multa que João deverá
+pagar. Imprima os dados do programa com as mensagens adequadas.
+"""
+
+limite = 50 
+multakg = 4.00 
+
+peso = float(input("Digite o peso de peixes trazido por João (em quilos): "))
+
+excesso = 0
+multa = 0
+
+if peso > limite:
+    excesso = peso - limite
+    multa = excesso * multakg
+
+if excesso > 0:
+    print(f"Excesso de peso: {excesso:.2f} quilos")
+    print(f"Multa a ser paga: R$ {multa:.2f}")
+else:
+    print("João não precisa pagar multa. Peso dentro do limite.")
